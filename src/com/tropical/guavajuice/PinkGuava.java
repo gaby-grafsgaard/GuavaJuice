f752ec8e50b17132b04b61eec9a1a3989ab02b3d
gaby-grafsgaard <gabyinhawaii@gmail.com>
2025-01-13 10:50:22 -1000

    Initialization code in GitHub

---
+++ src/com/tropical/guavajuice/PinkGuava.java
@@ -0,0 +1,10 @@
+package com.tropical.guavajuice;
+
+public class PinkGuava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Fresh guava juice");
+	}
+
+}
