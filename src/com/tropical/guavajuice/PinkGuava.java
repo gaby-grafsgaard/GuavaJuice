dc1f2d4a569561ccddcfeb712dfecc705288fdfb
gaby-grafsgaard <gabyinhawaii@gmail.com>
2025-01-13 10:56:32 -1000

    Have a cup

--- src/com/tropical/guavajuice/PinkGuava.java
+++ src/com/tropical/guavajuice/PinkGuava.java
@@ -5,6 +5,7 @@ public class PinkGuava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Fresh guava juice");
+System.out.println("Have a cup");
 	}
 
 }
